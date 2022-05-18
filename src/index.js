16f0f60a5f431c4e7c904698aa107d9f0e885427
VikashPR <vikashrajendran2806@gmail.com>
2022-05-18 16:40:27 +0530

    Test:  index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-function showAlert() {alert("Alert from JS file");}function updateHeading() {document.getElementById('heading').innerHTML = 'Heading changed with JS';}function showAlert() {alert("Alert from JS file");}function updateHeading() {document.getElementById('heading').innerHTML = 'Heading changed with JS';}
+function showAlert() {alert("Alert from JS file");}function updateHeading() {document.getElementById('heading').innerHTML = 'Heading changed with JS';}function showAlert() {alert("Alert from JS file");}function updateHeading() {
+    document.getElementById('heading').innerHTML = 'Heading changed with JS';}
