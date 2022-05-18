df8f2d392b3b25a9f9d9e4d7722820ca2d7a1c0c
VikashPR <vikashrajendran2806@gmail.com>
2022-05-18 16:38:45 +0530

    Feat: index.js update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
-console.log("mark");
-console.log("mark");
+function showAlert() {alert("Alert from JS file");}function updateHeading() {document.getElementById('heading').innerHTML = 'Heading changed with JS';}function showAlert() {alert("Alert from JS file");}function updateHeading() {document.getElementById('heading').innerHTML = 'Heading changed with JS';}
